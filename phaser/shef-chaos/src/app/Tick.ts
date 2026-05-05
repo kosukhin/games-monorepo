7bbab40362c8f20efc40d5396984350ca51b1e22
Eugene Kosukhin <kosuha606@gmail.com>
2026-05-05 11:46:47 +0300

    fix(sheff-chaos): command of main tick

--- phaser/shef-chaos/src/app/Tick.ts
+++ phaser/shef-chaos/src/app/Tick.ts
@@ -1,40 +1,19 @@
 import { CollisionsClean } from "@/app/CollisionsClean";
-import { CollisionWithTrashCan } from "@/app/CollisionWithTrashCan";
+import { CollisionWithMud } from "@/app/CollisionWithMud";
 import { CollisionWithTarakan } from "@/app/CollisionWithTarakan";
+import { CollisionWithTrashCan } from "@/app/CollisionWithTrashCan";
 import { GameOverGuard } from "@/app/GameOverGuard";
 import { LayerStateType } from "@/app/LayerState";
 import { TarakanMovement } from "@/app/TarakanMovement";
-import { BatchCommand, CommandType } from "silentium-loop";
-import { CollisionWithMud } from "@/app/CollisionWithMud";
+import { BatchCommand } from "silentium-loop";
 
 export function Tick(state: LayerStateType) {
-  const commands: CommandType[] = [];
-
-  commands.push({
-    type: "schedule",
-    next: GameOverGuard,
-  });
-
-  commands.push({
-    type: "schedule",
-    next: CollisionWithTrashCan,
-  });
-  commands.push({
-    type: "schedule",
-    next: CollisionWithTarakan,
-  });
-  commands.push({
-    type: "schedule",
-    next: CollisionWithMud,
-  });
-  commands.push({
-    type: "schedule",
-    next: CollisionsClean,
-  });
-  commands.push({
-    type: "schedule",
-    next: TarakanMovement,
-  });
-
-  return BatchCommand(state, commands);
+  return BatchCommand(state, [
+    { type: "schedule", next: GameOverGuard },
+    { type: "schedule", next: CollisionWithTrashCan },
+    { type: "schedule", next: CollisionWithTarakan },
+    { type: "schedule", next: CollisionWithMud },
+    { type: "schedule", next: CollisionsClean },
+    { type: "schedule", next: TarakanMovement },
+  ]);
 }
